c7768d98b381a3c4f18859cf53ebc79039060598
FlaviusGorgan <gorganflavius@gmail.com>
2021-05-23 12:54:37 +0300

    added gameDetailsPage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -9,18 +10,23 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public sort: String;
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string;
   public games: Array<Game>;
+
+  // catch subscriebers in aceste doua variabile
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(
     private httpService: HttpService,
-    private activatedRouter: ActivatedRoute
+    private activatedRouter: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     // check here first daca sunt search queries, si daca sunt vom face call la searchGames() cu ambii parametri
-    this.activatedRouter.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRouter.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -31,11 +37,28 @@ export class HomeComponent implements OnInit {
 
   // call la API sa facem fetch pe data requested
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
         console.log(gameList);
       });
+  }
+
+  // route application to the new page (details for the game page)
+  // crea detailsPage in components
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe;
+    }
+    if (this.gameSub) {
+      this.gameSub.unsubscribe;
+    }
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
   }
 }
